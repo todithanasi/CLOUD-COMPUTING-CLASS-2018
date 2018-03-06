a700bccb0dd8ccad014f7fb67c3dde9acdd6c78f
Noor Zehra <noor.zehra@skyelectric.com>
2018-03-06 22:51:06 +0100

    - Tokenize tweets with regular expressions to realize links, emoticons, numbers, special characters etc - Filter tweets by English Language.

--- Lab2/Twitter_2.py
+++ Lab2/Twitter_2.py
@@ -1,5 +1,7 @@
 import json
 import tweepy
+import re
+
 from tweepy import OAuthHandler
 
 consumer_key = '1deFGmrbgFbStqyL8cVpPzIxF'
@@ -12,18 +14,61 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
-# we use 1 to limit the number of tweets we are reading
-# and we only access the `text` of the tweet
-for status in tweepy.Cursor(api.home_timeline).items(1):
-    print(status.text)
+# # we use 1 to limit the number of tweets we are reading
+# # and we only access the `text` of the tweet
+# for status in tweepy.Cursor(api.home_timeline).items(1):
+#     print(status.text)
 
-# Tweets that the user can see on his Homepage. This are done by the accounts you are following.
-for status in tweepy.Cursor(api.home_timeline).items(10):
-    print(json.dumps(status._json, indent=2))
+# Tweets that the user can see on his Homepage.  These are the tweets by the accounts you are following.
+# for status in tweepy.Cursor(api.home_timeline).items(10):
+#     print(json.dumps(status._json, indent=2))
 
-for friend in tweepy.Cursor(api.friends).items(15):
-    print(json.dumps(friend._json, indent=2))
+# for friend in tweepy.Cursor(api.friends).items(15):
+#     print(json.dumps(friend._json, indent=2))
+#
+# # Tweets that the user does itself
+# for tweet in tweepy.Cursor(api.user_timeline).items(5):
+#     print(json.dumps(tweet._json, indent=2))
 
-# Tweets that the user does itself
-for tweet in tweepy.Cursor(api.user_timeline).items(5):
-    print(json.dumps(tweet._json, indent=2))
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>',  # HTML tags
+    r'(?:@[\w_]+)',  # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)",  # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+',  # URLs
+
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)',  # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])",  # words with - and '
+    r'(?:[\w_]+)',  # other words
+    r'(?:\S)'  # anything else
+]
+
+tokens_re = re.compile(r'(' + '|'.join(regex_str) + ')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^' + emoticons_str + '$', re.VERBOSE | re.IGNORECASE)
+
+
+def tokenize(s):
+    return tokens_re.findall(s)
+
+
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+
+
+# Take Tweets that the user can see on his Homepage. (These are the tweets by the accounts you are following).
+tweets = tweepy.Cursor(api.home_timeline).items(10)
+
+# Filter the tweets of English Language only
+for tweet in tweets:
+    if tweet.lang == "en":
+        print(preprocess(tweet.text))
